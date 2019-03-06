eb000c0f27776161a493dd3ae172aa100b7a8cf1
Dairon Leyva <dleyva@peopleticker.com>
2019-03-06 09:47:14 -0500

    verify categories

--- contentScriptAmazonProduct.js
+++ contentScriptAmazonProduct.js
@@ -66,6 +66,61 @@ function getInjectReferenceElement() {
   return injectReference;
 }
 
+const AMZ_ROOT_CATEGORIES = [
+  "Appliances",
+  "Arts, Crafts & Sewing",
+  "Automotive",
+  "Baby",
+  "Beauty & Personal Care",
+  "Books",
+  "Camera & Photo",
+  "Cell Phones & Accessories",
+  "Clothing, Shoes & Jewelry",
+  "Computers & Accessories",
+  "Electronics",
+  "Grocery & Gourmet Food",
+  "Health & Household",
+  "Home and Garden",
+  "Home & Kitchen",
+  "Industrial & Scientific",
+  "Jewelry",
+  "Kindle Store",
+  "Kitchen & Dining",
+  "Musical Instruments",
+  "Office Products",
+  "Patio, Lawn & Garden",
+  "Pet Supplies",
+  "Shoes",
+  "Software",
+  "Sports & Outdoors",
+  "Tools & Home Improvement",
+  "Toys & Games",
+  "Watches",
+  "Video Games"
+];
+
+function dataIsValid(category, bsr) {
+  if (!category) {
+    console.log("No category found!");
+    // TODO: show template "No category found for this product! report error"
+    return false;
+  }
+
+  if (!bsr) {
+    console.log("No BSR found!");
+    // TODO: show template "BSR not found for this product! report error"
+    return false;
+  }
+
+  if (category && !AMZ_ROOT_CATEGORIES.includes(category)) {
+    console.log(`No sales estimate for category: "${category}"`);
+    // TODO: show template "no sales estimate available"
+    return false;
+  }
+
+  return true;
+}
+
 async function main() {
   console.log("executing");
   const productInfoElement = getProductInfoElement();
@@ -74,7 +129,7 @@ async function main() {
 
   const bsrText = productInfoElement.textContent.replace(/\s+/g, " ");
   // find category
-  const categoryFinder = /(?:Best Sellers Rank:? )(?:(?:\#)(?:\d{1,3})(?:,?\d{3})* in )(.+?)(?:\s\(|\s>)/;
+  const categoryFinder = /(?:Best Sellers Rank:? )(?:(?:\#)(?:\d{1,3})(?:,?\d{3})* in )(.+?)(?:\s\(|\s>|\s#|\sDate first listed on Amazon)/;
   const categoryMatch = categoryFinder.exec(bsrText);
   console.log("categoryMatch:", categoryMatch);
   const category = categoryMatch.length > 1 ? categoryMatch[1] : null;
@@ -85,15 +140,23 @@ async function main() {
   const bsr = bsrMatch.length > 1 ? bsrMatch[1].replace(/,/g, "") : null;
   console.log("bsr:", bsr);
 
-  if (category && bsr) {
-    // const sales = await fakeGetSales(category, bsr);
-    const sales = await getSalesEstimate(category, bsr);
-    console.log("sales:", sales);
-    const template = await getTemplate();
-    const injectElement = createElementFromHTML(template.replace("{ESTIMATED_SALES}", sales));
-    const injectReference = getInjectReferenceElement();
-    insertBefore(injectElement, injectReference);
+  if (!dataIsValid(category, bsr)) return;
+
+  // const sales = await fakeGetSales(category, bsr);
+  const sales = await getSalesEstimate(category, bsr);
+  console.log("sales:", sales);
+
+  if (!sales) {
+    console.log("No sales estimate available");
+    // TODO: show template "no sales estimate available"
+    return;
   }
+
+  // TODO: implement template parsing function using regular expressions
+  const template = await getTemplate();
+  const injectElement = createElementFromHTML(template.replace("{ESTIMATED_SALES}", sales));
+  const injectReference = getInjectReferenceElement();
+  insertBefore(injectElement, injectReference);
 }
 
 main();
